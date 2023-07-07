81f0109a9d5482f995dd5de30bf6c4a853857de3
EcuacionCuadratica.java <2019110470@udh.edu.pe>
2023-07-06 20:03:33 -0500

    hito 2

--- app/src/main/java/com/example/amovcuartatarea/MainActivity.java
+++ app/src/main/java/com/example/amovcuartatarea/MainActivity.java
@@ -4,18 +4,44 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    EditText et1;
+    EditText et2;
+
+    Button  button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        et1 = findViewById(R.id.et1);
+        et2 = findViewById(R.id.et2);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Bundle datos = new Bundle();
+                datos.putString("Datos", et1.getText().toString());
+                datos.putInt("Datos1", Integer.parseInt(et2.getText().toString()));
+
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                intent.putExtras(datos);
+                startActivity(intent);
+            }
+        });
 
         Toolbar tarea4 = findViewById(R.id.myToolbar);
         setSupportActionBar(tarea4);
@@ -23,26 +49,6 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.mymenu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        int id = item.getItemId();
-        if (id == R.id.opcion1){
-            Toast.makeText(getApplicationContext(), "Icono 1", Toast.LENGTH_SHORT).show();
-        }
-
-        if (id == R.id.opcion2){
-            finish();
-            return true;
-        }
 
 
-        return super.onOptionsItemSelected(item);
-    }
 }
--- app/src/main/java/com/example/amovcuartatarea/MainActivity2.java
+++ app/src/main/java/com/example/amovcuartatarea/MainActivity2.java
@@ -1,14 +1,68 @@
 package com.example.amovcuartatarea;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity2 extends AppCompatActivity {
+
+    TextView tvResultado;
+    TextView resultado2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        tvResultado =findViewById(R.id.tvResultado);
+        resultado2 = findViewById(R.id.resultado2);
+
+        Bundle recibirDatos = getIntent().getExtras();
+        String info = recibirDatos.getString("Datos");
+        int info1 = recibirDatos.getInt("Datos1");
+
+        tvResultado.setText(info);
+        resultado2.setText(String.valueOf(info1));
+
+        Toolbar tarea4 = findViewById(R.id.myToolbar);
+        setSupportActionBar(tarea4);
+        getSupportActionBar().setTitle("");
+
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.mymenu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.opcion1){
+            Toast.makeText(getApplicationContext(), "Icono 1", Toast.LENGTH_SHORT).show();
+        }
+
+        if (id == R.id.opcion3){
+            Toast.makeText(getApplicationContext(), "Icono 3", Toast.LENGTH_SHORT).show();
+        }
+
+        if (id == R.id.opcion2){
+            finish();
+            return true;
+        }
+
+
+        return super.onOptionsItemSelected(item);
     }
 }
